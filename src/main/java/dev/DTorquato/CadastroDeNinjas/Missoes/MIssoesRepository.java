536644f81120c6c5b06442f93678e81f2258314d
Danilo Torquato <d.torquato.dev@gmail.com>
2025-07-20 09:02:46 -0300

    feat: Implementa novas classes

---
+++ src/main/java/dev/DTorquato/CadastroDeNinjas/Missoes/MIssoesRepository.java
@@ -0,0 +1,6 @@
+package dev.DTorquato.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MIssoesRepository  extends JpaRepository<MissoesModel, Long> {
+}
