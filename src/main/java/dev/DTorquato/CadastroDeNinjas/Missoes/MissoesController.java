de9ad7a5d20856a8f19677fee16a2f130559d25b
Danilo Torquato <d.torquato.dev@gmail.com>
2025-07-21 12:15:17 -0300

    feat: criar as rotas com mock

---
+++ src/main/java/dev/DTorquato/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,31 @@
+package dev.DTorquato.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String mostrarMissoes() {
+        return "Misões listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String adeletarMissao(){
+        return "Missao deletada";
+    }
+
+
+
+}
