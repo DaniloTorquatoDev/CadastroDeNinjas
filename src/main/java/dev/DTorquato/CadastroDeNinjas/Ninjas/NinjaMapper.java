48e9c8e71be0945f9aad3fad366095c4e0e417eb
Danilo Torquato <d.torquato.dev@gmail.com>
2025-07-25 08:02:38 -0300

    chore: inclue a versão do DTO pela entidade e mapeia manualmente no ninjaMapper os caminhos entida x dto

--- src/main/java/dev/DTorquato/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/DTorquato/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,36 @@
 package dev.DTorquato.CadastroDeNinjas.Ninjas;
 
+import dev.DTorquato.CadastroDeNinjas.Missoes.MissoesModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjasDTO ninjasDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjasDTO.getId());
+        ninjaModel.setNome(ninjasDTO.getNome());
+        ninjaModel.setEmail(ninjasDTO.getEmail());
+        ninjaModel.setIdade(ninjasDTO.getIdade());
+        ninjaModel.setMissoes(ninjasDTO.getMissoes());
+        ninjaModel.setRank(ninjasDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public NinjasDTO map(NinjaModel ninjaModel) {
+        NinjasDTO ninjasDTO = new NinjasDTO();
+
+        ninjasDTO.setId(ninjaModel.getId());
+        ninjasDTO.setNome(ninjaModel.getNome());
+        ninjasDTO.setEmail(ninjaModel.getEmail());
+        ninjasDTO.setIdade(ninjaModel.getIdade());
+        ninjasDTO.setMissoes(ninjaModel.getMissoes());
+        ninjasDTO.setRank(ninjaModel.getRank());
+
+        return ninjasDTO;
+
+    }
+
 }
--- src/main/java/dev/DTorquato/CadastroDeNinjas/Ninjas/NinjasDTO.java
+++ src/main/java/dev/DTorquato/CadastroDeNinjas/Ninjas/NinjasDTO.java
@@ -1,4 +1,22 @@
 package dev.DTorquato.CadastroDeNinjas.Ninjas;
 
+import dev.DTorquato.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjasDTO {
-}
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String nome;
+        private String email;
+        private int idade;
+        private MissoesModel missoes;
+        private String rank;
+    }
+
+
